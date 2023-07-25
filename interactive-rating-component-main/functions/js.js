84c02c3a78e462a605d0505d22dceabb109256e8
MikeZeg <michalzegzula@gmail.com>
2023-07-25 18:24:58 +0100

    add JS - repnse - color rating change - done

--- interactive-rating-component-main/functions/js.js
+++ interactive-rating-component-main/functions/js.js
@@ -1,4 +1,27 @@
 // alert('Working');
 
-const digit = document.querySelectorAll('digit')
+//hooks
+const btnQuestion = document.querySelector('#btnSubmit');
+let myDigit = document.querySelectorAll('.digit');
 
+// import styles from '../styles/styles.css';
+
+
+// mark digit
+myDigit.forEach(function(elem){
+
+    elem.addEventListener("click", function(){
+        myDigit.forEach(function(backTo){
+            backTo.style = "background-color:$darkBlue; color:$lightGrey";
+        });
+        this.style = "background-color:grey; color:white";
+        // this.style.backgroundColor = styles.lightGrey;
+        // this.style.color = styles.myWhite;
+    })
+})
+
+// Change to thank you state
+btnQuestion.addEventListener('click', function(){
+    console.log('mam');
+    
+})
