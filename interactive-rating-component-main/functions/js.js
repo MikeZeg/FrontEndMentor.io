e818d256615d72b3a412cf37d57a420bc79aa734
MikeZeg <michalzegzula@gmail.com>
2023-07-26 20:16:04 +0100

    layout changes

--- interactive-rating-component-main/functions/js.js
+++ interactive-rating-component-main/functions/js.js
@@ -3,36 +3,41 @@
 // ---------------------------------------------
 
 //Selected Digit
-const selectedDigit = '0';
-const 
-
+    const selectedDigit = [];
+    // const addText = document.getElementsByClassName('answer_rating');
+    const addText = document.getElementById('rating');
 // mark digit
    //hooks
+    const question = document.querySelector('.question');
+    const answer  = document.querySelector('.answer');
     const btnQuestion = document.querySelector('#btnSubmit');
     let myDigit = document.querySelectorAll('.digit');
 
-myDigit.forEach(function(elem){
+//Change styles ratings span
+    myDigit.forEach(function(elem){
 
-    elem.addEventListener("click", function(){
-        myDigit.forEach(function(backTo){
-            backTo.style = "background-color:$darkBlue; color:$lightGrey";
-        });
-        this.style = "background-color:grey; color:white";
-        // this.style.backgroundColor = styles.lightGrey;
-        // this.style.color = styles.myWhite;
+        elem.addEventListener("click", function(){
+            myDigit.forEach(function(backTo){
+                backTo.style = "background-color:$darkBlue; color:$lightGrey";
+            });
+            this.style = "background-color:grey; color:white";
+            
+            selectedDigit.slice(1)
+            selectedDigit.push(this.innerText);
+            // console.log(this.innerText);
+            // console.log(selectedDigit);
+        })
     })
-})
 
 // Change to thank you state
     //hooks
-    const question = document.querySelector('.question');
-    const answer  = document.querySelector('.answer');
+    btnQuestion.addEventListener('click', function(){
 
-
-
-btnQuestion.addEventListener('click', function(){
-
-    question.style.display = 'none';
-    answer.style.display = 'grid';
-    
-})
+        if(selectedDigit.length === 0){
+            alert('Please select Rating')
+        }else{
+            question.style.display = 'none';
+            answer.style.display = 'grid';
+            addText.innerText = 'You selected ' + selectedDigit[selectedDigit.length - 1] + ' out of 5';
+        }
+    })
