911cc0ce29707fd922ede02b4537f14b090ab3f3
MikeZeg <michalzegzula@gmail.com>
2023-09-08 19:47:56 +0100

    JS display main content

--- newsletter-sign-up-with-success-message-main/js/script.js
+++ newsletter-sign-up-with-success-message-main/js/script.js
@@ -1,5 +1,7 @@
 //grab all section - main, hidden_page, email_input, button
-const mainPage = document.querySelector('.main');
+const firstPart = document.querySelector('.main__firstPart');
+const figurePart = document.querySelector('.main__figure');
+
 const success = document.querySelector('.success');
 const email = document.getElementById('email_input');
 const btnEmail = document.querySelector('#btnEmail');
@@ -16,6 +18,7 @@ const figure = document.querySelector('figure');
 function contentChange (){
 
     console.log('work')
-//     mainPage.classList.add(hidde__style);
-//     success.classList.add(display__style);
-// }
+    firstPart.style.display = 'none';
+    figurePart.style.display = 'none';
+    success.style.display = 'flex';
+}
