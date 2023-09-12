2ad7006e799dc9bce736bcd51014dfecc7075c3f
MikeZeg <michalzegzula@gmail.com>
2023-09-12 15:00:15 +0100

    JS - add features - error, vilid required email

--- newsletter-sign-up-with-success-message-main/js/script.js
+++ newsletter-sign-up-with-success-message-main/js/script.js
@@ -1,38 +1,42 @@
 //grab all section - main, hidden_page, email_input, button
+const main = document.querySelector('main')
 const firstPart = document.querySelector('.main__firstPart');
 const figurePart = document.querySelector('.main__figure');
 const email = document.querySelector('#email_input');
 const btnEmail = document.querySelector('#btnEmail');
+const errorNote = document.querySelector('.main__secondPart__confirm__text span');
 
 const success = document.querySelector('.success');
 const successEmail = document.querySelector('.success__info span');
 
 // magic making: 
-//  done 1) clear email when selected
-//  2) change button color
-//  done 3) Button press hide main content and add success content
 
-email.addEventListener('click', function(){
-    console.log('Access')
-    email.value = ''
-});
+    email.addEventListener('click', function(){
+        console.log('Access')
+        email.value = ''
+    });
 
-// done 1) check the correctness of email
-// done 2) add email value to hidden page 
-function contentChange (){
-    const checkEmail = email.value;
-    const mailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-    const easyMailFormat = /^\S+@\S+\.\S+$/;
+    function contentChange (){
+        const checkEmail = email.value;
+        const mailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+        const easyMailFormat = /^\S+@\S+\.\S+$/;
 
-//check email format
-    if(checkEmail.match(easyMailFormat)){
-        console.log('work');
-    
-        firstPart.style.display = 'none';
-        figurePart.style.display = 'none';
-        success.style.display = 'flex';
-        successEmail.textContent = checkEmail;
-    }else{
-        alert('Please add correct Email. '+ checkEmail)
+    //check email format
+        if(checkEmail.match(easyMailFormat)){
+            console.log('work');
+        
+            main.style.minWidth = '25rem';
+            main.style.maxWidth = '26rem'
+            main.style.minHeight = '26rem';
+            main.style.maxHeight = '27rem'
+
+            firstPart.style.display = 'none';
+            figurePart.style.display = 'none';
+            success.style.display = 'flex';
+            successEmail.textContent = checkEmail;
+        }else{
+            alert('Please add correct Email. '+ checkEmail)
+            
+
+        }
     }
-}
