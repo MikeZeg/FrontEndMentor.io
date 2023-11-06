58201965c89a53665c82c6b3931144817d3a0655
MikeZeg <michalzegzula@gmail.com>
2023-11-06 15:49:03 +0000

    add JS - api to url

---
+++ advice-generator-app-main/app/JS/interactive.js
@@ -0,0 +1,42 @@
+// alert('working ?')
+// console.log('connect ')
+
+// API -> URL
+const urlApi = "https://api.adviceslip.com/advice";
+
+// Advice - value change
+const adviceNumber = document.querySelector('.advice__card__container__number');
+const adviceText = document.querySelector('.advice__card__container__text');
+
+// Btn
+const button = document.querySelector('.btn');
+
+
+
+// Function/s
+const btnPress = async()=>{
+    console.log('Working')
+
+    const res = await fetch(urlApi);
+    const data = await res.json()
+
+    const slipId = data.slip.id;
+    const slipText = data.slip.advice;
+
+    console.log(data);
+    console.log(slipId);
+
+    adviceNumber.innerHTML = slipId;
+    // or advceNumber.innerHTML = `Advice #${slipId}`
+    adviceText.innerHTML = slipText;
+    // or adviceText.innerHTML = `"${slipText}"`
+}
+
+// check function/s
+
+    function checkFun (){
+        btnPress()
+
+        setTimeout(checkFun,3000);
+    }
+    checkFun();
