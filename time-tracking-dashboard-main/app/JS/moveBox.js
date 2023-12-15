cd559db81781676f0a29b9c0b8ff788034560b90
MikeZeg <michalzegzula@gmail.com>
2023-12-15 19:11:44 +0000

    JS try to catch style.gridarea

--- time-tracking-dashboard-main/app/JS/moveBox.js
+++ time-tracking-dashboard-main/app/JS/moveBox.js
@@ -1,7 +1,6 @@
 // global
 let active = false;
 let dragElement ;
-console.log(dragElement);
 
 // TASK
 // check transferData to move only grid-area CSS
@@ -16,6 +15,16 @@ const selfCareBox = document.querySelector('.selfCare');
 
 const boxArr = [workBox, playBox, studyBox, exerciseBox,socialBox,selfCareBox]
 
+// boxes to grid
+const work = workBox.style.gridArea
+const play = playBox.style.gridArea
+const study = studyBox.style.gridArea
+const exercise = exerciseBox.style.gridArea
+const social = socialBox.style.gridArea
+const selfCare = selfCareBox.style.gridArea
+
+const boxGridArr = [work, play, study, exercise, social, selfCare]
+
 // <--------- Grab Elements -------------------------------->
     let items = document.querySelectorAll('.grab')
     items.forEach((item)=> item.addEventListener('click', pressDotts))
@@ -24,46 +33,27 @@ const boxArr = [workBox, playBox, studyBox, exerciseBox,socialBox,selfCareBox]
     function pressDotts(e) {
 
         if(active != true) {
-            boxArr.forEach((box)=>{
-                box.setAttribute('draggable', true);
-
-//<--- Boxes ------ Boxes ------ Boxes ------ Boxes --->
-            box.addEventListener('dragstart', onDragStart);
-            box.addEventListener('dragend', dragEnd)
-            box.addEventListener('dragenter', dragEnter );
-            box.addEventListener('dragleave', dragleave);
-            box.addEventListener('dragover', onDragOver); //check
-            box.addEventListener('drop', onDrop); //NOK
-//<------ End ------ End ------ End ------ End --->
-        })
-        
-        items.forEach((item)=>{
-            item.innerHTML = 'unlocked';
-            item.classList.add('pressDotts');
-        });
-        active = true;
-        // console.log('after press: ' + active)
-    }else {
-        boxArr.forEach((item)=>{item.setAttribute('draggable', false);})
-        // Plus info ?
-        items.forEach((item)=>{
-            item.innerHTML = '...';
-            item.classList.remove('pressDotts');
-            active = false;
-        });
+            boxActive();
+//Dotts Active
+            dottsActive();
+            active = true;
+// console.log('after press: ' + active)
+        }else {
+            boxUnActive();
+//Dotts UnActive ?
+            dottsUnActive();
+        }
     }
-}
-
 
 // <--------------- Supporting functions ------------------->
 function onDragStart(e) {
     this.style.opacity = '0.4';
-
+    console.log(this.style.gridArea)
     dragElement = this;
-    
+
     e.dataTransfer.effectAllowed = 'move';
     e.dataTransfer.setData('text/html',this.innerHTML)
-
+    // e.dataTransfer.setData('text/html',this.style.gridArea);
 }
 
 function dragEnd(e) {
@@ -93,18 +83,55 @@ function onDragOver(e) {
 }
 
 function onDrop(e) {
-    
     boxArr.forEach((box)=> {
         box.style.opacity= '1';
         box.classList.remove('over');
     })
-    console.log(this.innerHTML)
+    // console.log(this.innerHTML)
     e.stopPropagation();
 
     if(dragElement != this) {
         dragElement.innerHTML = this.innerHTML;
         this.innerHTML = e.dataTransfer.getData('text/html')
-    }
 
+        // this.target.appendChild(getData)
+    }
     return false;
+}
+
+function dottsActive() {
+
+    items.forEach((item)=>{
+            item.innerHTML = 'unlocked';
+            item.classList.add('pressDotts');
+        });
+}
+
+function dottsUnActive() {
+
+    items.forEach((item)=>{
+            item.innerHTML = '...';
+            item.classList.remove('pressDotts');
+            active = false;
+        });
+}
+
+function boxActive() {
+    boxArr.forEach((box)=>{
+                box.setAttribute('draggable', true);
+//<--- Boxes ------ Boxes ------ Boxes ------ Boxes --->
+            box.addEventListener('dragstart', onDragStart);
+            box.addEventListener('dragend', dragEnd)
+            box.addEventListener('dragenter', dragEnter );
+            box.addEventListener('dragleave', dragleave);
+            box.addEventListener('dragover', onDragOver);
+            box.addEventListener('drop', onDrop);
+//<------ End ------ End ------ End ------ End --->
+        })
+}
+
+function boxUnActive() {
+    boxArr.forEach((item)=>{
+        item.setAttribute('draggable', false);
+    });
 }
