b97012c98102d73cd15173d53c8b1608c4ded7e3
MikeZeg <michalzegzula@gmail.com>
2024-01-15 05:11:11 +0000

    deploy issue - git puhs error

---
+++ README.md
@@ -0,0 +1,49 @@
+## 👋 All.
+### This is a solution to the Blog Preview Card. [Frontend Mentor](https://www.frontendmentor.io) - Please be happy to check challengs, that help you improve your coding skills by building realistic projects. 
+
+### Table of contents
+
+- [Preview](#overview)
+    - [The Challenge](#The-challenge)
+    - [Link](#Links)
+    - [Used Technology](#Used-Technology)
+    - [Performance](##Performance)
+        - [Dektop](###Desktop)
+        - [Mobile](###Mobile)
+- [Author](#Author)
+
+### Preview
+
+![Preview](./design/desktop-preview.jpg)
+
+### The Challenge
+
+Your challenge is to build out this Newsletter project component and get it looking as close to the design as possible.
+
+Users should be able to:
+
+- See hover and focus states for all interactive elements on the page
+
+### Links
+
+[live](https://mikezeg.github.io/FrontEndMentor.io/blog-preview-card/)
+
+### Used Technology
+
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white) ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![SASS](https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white)
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)
+
+### Used Tools
+![VS Code](https://img.shields.io/badge/VS%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white) ![Google](https://img.shields.io/badge/google-DA4437?style=for-the-badge&logo=google&logoColor=white) ![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)
+
+### Performance
+
+  #### Desktop
+![deskopt-performance](./design/)
+
+  #### Mobile
+![mobile-performance](./design/)
+
+### Author 🚀
+ -- FrontEnd Mentor -> [MikeZeg](https://www.frontendmentor.io/profile/MikeZeg)
---
+++ src/index.js
@@ -0,0 +1,190 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import logoImage from './assets/images/illustration-article.svg';
+import userLogo from './assets/images/image-avatar.webp'
+
+// Root Css
+// const rootStyle = {
+//   backgroundColor: 'hsl(47, 88%, 63%)',
+//   fontSize: '1rem',
+// }
+const user = {
+  firstName: 'Greg',
+  lastName: 'Hooper',
+}
+
+const MainCard = () => {
+  const styleCard={
+    display: 'grid',
+    position: 'absolute', left: '50%', top: '50%',
+    transform: 'translate(-50%, -50%)',
+
+    border: '1px hsl(0,0%,7%) solid',
+
+    backgroundColor: 'hsl(0,0%,100%)',
+    minWidth: '380px',
+    maxWidth: '20%',
+    minHeight: '520px',
+    maxHeing: '35%',
+
+    boxShadow: '15px 15px 1px 1px black',
+    borderRadius: '25px',
+    shadowColor: 'hsl(0,0%,7%)'
+  }
+
+// Figure - Style -
+  const figStyle = {
+    margin: '25px 20px auto 20px',
+    padding: '0',
+    borderRadius:'10px',
+
+    backgroundImage: `url(${logoImage})`,
+    width:'90%',
+    height:'200px',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  }
+
+// Content - Style -
+const contentStyle = {
+    margin:'0 auto auto 25px',
+  }
+const contentStyle__article = {
+
+}
+const contentStyle__article__h1 = {
+  fontSize: '25px',
+  margin: '10px auto 15px auto',
+  // marginTop: '10px',
+}
+const contentStyle__article__h2 = {
+  fontSize: '18px',
+  fontWeight: '300',
+  color: 'hsl(0,0%,50%)',
+
+}
+
+// User - Info - Style -
+   const  userInfoStyle = {
+    margin:'0',
+    
+    width: '80%',
+    height: '30px',
+
+    marginLeft: '25px',
+    marginRight: '25px',
+    display: 'flex',
+   }
+   const userInfoStyle__Img = {
+    display: 'inline-block',
+    margin: '0',
+
+    backgroundImage: `url(${userLogo})`,
+    width: '30px',
+    height: '30px',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+ }
+  const userInfoStyle__h1 = {
+    display: 'inline',
+    margin: 'auto auto auto 10px',
+    fontSize: '14px',
+ }
+
+// Blog - style -
+   const blogInfoStyle = {
+    margin:'0 auto auto 25px',
+  }
+   const blogInfoStyle__boxYellow = {
+    margin: '20px 0 20px 0',
+    width: '50%',
+
+    backgroundColor: 'hsl(47,88%,63%)',
+    borderRadius: '5px',
+    height:'30px',
+    fontSize: '14px',
+   }
+   const blogInfoStyle__boxYellow__text = {
+      margin: 0,
+      height: '100%',
+      width: '100%',
+      display: 'flex',
+    
+      alignItems: 'center',
+      justifyContent: 'center',
+   }
+
+// InLine - Style -
+   const inlineStyle = {
+    display: 'inline',
+    width: '100px',
+    height: '50px',
+   }
+   
+
+  return (
+    <main style={styleCard}>
+
+      <figure style={figStyle}>
+        {/* <img src={logoImage} alt='blog_logo' style={styleImg}></img> */}
+        <figcaption></figcaption>
+      </figure>
+
+    <section style={blogInfoStyle}>
+      <h1 style={blogInfoStyle__boxYellow}> <p style={blogInfoStyle__boxYellow__text}>Learning</p></h1>
+      <time style={inlineStyle}><p style={inlineStyle}>Published:</p> 21 Dec 2023</time>
+    </section>
+
+    <section style={contentStyle}>    
+      <article style={contentStyle__article}>
+        <h1 style={contentStyle__article__h1}> HTML & CSS fundations</h1>
+        <h2 style={contentStyle__article__h2}>These languages are the backbone of every website, defining structure, content and presentation.</h2>
+      </article>
+    </section>
+
+    <section style = {userInfoStyle}>
+      <figure style={userInfoStyle__Img}></figure><h1 style={userInfoStyle__h1}>{user.firstName} {user.lastName}</h1>
+    </section>
+
+    </main>
+  )
+}
+
+const Footer = () => {
+  const footerStyle = {
+    position: 'absolute',
+    bottom: '0',
+    width: '100%',
+    textAlign: 'center',
+  }
+  const footerStyle__div = {
+    color: 'hsl(0,0%,7%)',
+  }
+  const footerStyle__a__frontEnd = {
+    textDecoration: 'none',
+    color: 'hsl(0,0%,7%)',
+  }
+  const footerStyle__a__mikeZeg = {
+    textDecoration: 'none',
+    color: 'hsl(0,0%,7%)',
+  }
+  return (
+    <footer style={footerStyle}>
+      <div style={footerStyle__div}> Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" style={footerStyle__a__frontEnd}> Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/MikeZeg" target="_blank" style={footerStyle__a__mikeZeg}> MikeZeg</a>.
+      </div>
+  </footer>
+  )
+}
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root'));
+
+  root.render(
+    <div>
+    <MainCard/>
+    <Footer/>
+    </div>
+);
