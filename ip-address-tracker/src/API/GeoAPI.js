51e17d89c772717bf22d726f40781cbfcc4bf964
MikeZeg <michalzegzula@gmail.com>
2024-02-28 08:29:44 +0000

    API reload new data issue

--- ip-address-tracker/src/API/GeoAPI.js
+++ ip-address-tracker/src/API/GeoAPI.js
@@ -1,19 +1,87 @@
 import { API_KEY }  from '../component/config'
+import { useState , setState, useCallback } from 'react';
+
+
 
 // API url
-// const urlWeb = `https://ipapi.co/`;
-
-
+const urlWeb = `https://ipapi.co/`;
 const urlGeoApi = `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${API_KEY}`
 const urlCountry = `https://geo.ipify.org/api/v2/country?apiKey=${API_KEY}`;
 
 const myURL = 'https://jsonplaceholder.typicode.com/todos/1';
 
-// API GEO
+export const myData = {
+    ip:'123.31.3.3',
+    location: {
+        region:"Warmia",
+        city:'Elblag',
+        country_name:'Poland',
+        timezone:'+1',
+        lat: '50',
+        lng: '20'
+    },
+    isp:'3 mobile'
+}
 
+
+// API GEO
 // const response = await fetch('https://jsonplaceholder.typicode.com/users/1')
 // const response = await fetch(urlGeoApi)
-const myData = await response.json();
+// const myData = await response.json();
 
+// ----- Function Button  ------
+export const buttonAPI = (val,e) => {
+
+    const startMyData = {
+        ip:'',
+        location: {
+            region:"",
+            city:'',
+            country_name:'',
+            timezone:'',
+            lat: '',
+            lng: ''
+        },
+        isp:''
+    }
+
+    const [newData, setNewData] = useState(startMyData)
+
+    e.preventDefault();
+    console.log('Button Pressed');
+    console.log('Value send to GeoAPI: ',val);
+
+    let link = `https://ipapi.co/${val}/json/`
+    fetch(link)
+        .then((response)=> response.json())
+        .then((res) => {
+            const {
+                city,
+                country_name,
+                ip,
+                latitude,
+                longitude,
+                utc_offset,
+                region,
+                asn,
+            } = res
+            myData.ip = ip
+            myData.location.lat = latitude
+            myData.location.lng = longitude
+            myData.location.city = city;
+            myData.location.country_name = country_name;
+            myData.location.region = region;
+            myData.location.timezone = utc_offset;
+            
+
+            if(asn) {
+                console.log('ASN: ',asn)
+            }
+
+            console.log("dziala: ",myData)
+            
+        })
+        .catch((error)=>console.log("Connection Issues, please check require."))
+}
 
 export default myData
--- ip-address-tracker/src/component/Header.js
+++ ip-address-tracker/src/component/Header.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
 import '../styles/headerStyle.css'
 import myData from '../API/GeoAPI.js'
+import { buttonAPI } from '../API/GeoAPI.js'
 import img from '../images/icon-arrow.svg'
 
 console.log(myData)
 
-export function InputForm() {
+export function InputForm(props) {
+     
     const [ipAddress, setIpAddress] = useState("");
+    
+    console.log(ipAddress)
+    
     return (
         <form className='form'>
                     <input
@@ -16,10 +21,9 @@ export function InputForm() {
                         placeholder='Search for any IP address or domain'
                         onChange={(e)=> setIpAddress(e.target.value)}
                     />
-                    <button type='submit' className='form__btn btn'>
+                    <button className='form__btn btn' onClick={(e)=> buttonAPI(ipAddress,e)} type='subit'>
                         <img className='form__btn__img img' src={img} alt="arrow image" />
                     </button>
-                    
         </form>
     )}
 
@@ -34,9 +38,9 @@ class Header extends React.Component {
 {/* ------- Input ------- */}
                 <InputForm/>
                 <section className='headerStyle__info'>
-                    <h2 className='headerStyle__info__ip box'><span className='top__span'>IP ADDRESS </span><p className='headerStyle___info__IP__p'>{myData.as.route}</p></h2>
-                    <h2 className='headerStyle__info__location box box__border'>LOCATION <p className='headerStyle__info__location__p'>{myData.location.region}, {myData.location.city}</p></h2>
-                    <h2 className='headerStyle__info__time box box__border'>Time ZONE <p className='headerStyle__info__time__p'>{myData.timezone}</p></h2>
+                    <h2 className='headerStyle__info__ip box'><span className='top__span'>IP ADDRESS </span><p className='headerStyle___info__IP__p'>{myData.ip}</p></h2>
+                    <h2 className='headerStyle__info__location box box__border'>LOCATION <p className='headerStyle__info__location__p'>{myData.location.city}, {myData.location.region}, {myData.location.country_name}</p></h2>
+                    <h2 className='headerStyle__info__time box box__border'>Time ZONE <p className='headerStyle__info__time__p'>{myData.location.timezone}</p></h2>
                     <h2 className='headerStyle__info__isp box box__border'>ISP <p className='headerStyle__info__isp__p'>{myData.isp}</p></h2>
                 </section>
             </header>
