1a58c15d6039e3c11dd9c9bbb5dde2d448038035
MikeZeg <michalzegzula@gmail.com>
2024-09-26 15:32:06 +0100

    add components, struc application

---
+++ rest-countries-api/src/component/LandingPaga.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+// import '../style/landingPageStyle.css'
+
+const LandingPage = (props) => {
+
+    return (
+        <main>
+            <Header/>
+            <Search/>
+            <MainContent/>
+        </main>
+    )
+}
+
+export default LandingPage;
